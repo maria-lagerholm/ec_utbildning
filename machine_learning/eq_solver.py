23dfac62c9b5029296fd2b337fe1286362aa09ca
Maria Lagerholm <mkru@gmx.com>
2025-03-17 10:35:41 +0100

    theme update

--- machine_learning/eq_solver.py
+++ machine_learning/eq_solver.py
@@ -20,7 +20,7 @@ st.markdown("""
     width: 100% !important;
     height: auto !important;
 }
-@media (max-width: 1000px) {
+@media (max-width: 1024px) {
     [data-testid="stCanvas"] > canvas {
         width: 50% !important;
         height: auto !important;
